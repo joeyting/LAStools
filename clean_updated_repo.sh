44f18fcef45ca8a7174f4ca0adad87749897914c
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-03-29 12:03:55 +0200

    Clean a bit more the repository

--- clean_updated_repo.sh
+++ clean_updated_repo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-git rm -r -f ArcGIS_toolbox/ bin data example_batch_scripts/ IMAGINE_toolbox/ HALL_OF_SHAME.txt lastools.dsw Makefile QGIS_toolbox/ src/ README.txt
+git rm -r -f ArcGIS_toolbox/ bin data example_batch_scripts/ IMAGINE_toolbox/ HALL_OF_SHAME.txt lastools.dsw Makefile QGIS_toolbox/ src/ README.txt LASlib/LASlib.dsp LASlib/LASlib.dsw  LASlib/lib/ LASlib/Makefile LASlib/README.txt LASlib/example/lasexample_add_rgb.dsp LASlib/example/lasexample.dsp LASlib/example/lasexample_write_only.dsp LASzip/LASzip.dsp LASzip/LASzip.dsw LASzip/Makefile LASzip/README.txt LASzip/dll/ LASzip/example/laszipdllexample.dsp LASzip/example/laszippertest.dsp
