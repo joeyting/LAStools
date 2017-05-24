f05daa80de07bd539bec4500a56e8c2db1576ffc
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-05-24 10:57:53 +0200

    Update cleaning script

--- clean_updated_repo.sh
+++ clean_updated_repo.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/sh
 
-git rm -r -f ArcGIS_toolbox/ bin data example_batch_scripts/ IMAGINE_toolbox/ HALL_OF_SHAME.txt lastools.dsw Makefile QGIS_toolbox/ src/ README.txt LASlib/LASlib.dsp LASlib/LASlib.dsw  LASlib/lib/ LASlib/Makefile LASlib/README.txt LASlib/example/lasexample_add_rgb.dsp LASlib/example/lasexample.dsp LASlib/example/lasexample_write_only.dsp LASzip/LASzip.dsp LASzip/LASzip.dsw LASzip/Makefile LASzip/README.txt LASzip/dll/ LASzip/example/laszipdllexample.dsp LASzip/example/laszippertest.dsp
+git rm -r ArcGIS_toolbox/
+git rm -r IMAGINE_toolbox/
+git rm -r LASlib/lib/
+git rm -r LASzip/dll/
+git rm -r QGIS_toolbox/
+git rm -r bin/
+git rm -r data/
+git rm -r example_batch_scripts/
+git rm -r src/
+git rm HALL_OF_SHAME.txt
+git rm LASlib/LASlib.dsp
+git rm LASlib/LASlib.dsw
+git rm LASlib/Makefile
+git rm LASlib/README.txt
+git rm LASlib/example/lasexample.dsp
+git rm LASlib/example/lasexample_add_rgb.dsp
+git rm LASlib/example/lasexample_write_only.dsp
+git rm LASzip/LASzip.dsp
+git rm LASzip/LASzip.dsw
+git rm LASzip/Makefile
+git rm LASzip/README.txt
+git rm LASzip/example/laszipdllexample.dsp
+git rm LASzip/example/laszippertest.dsp
+git rm Makefile
+git rm README.txt
+git rm lastools.dsw
