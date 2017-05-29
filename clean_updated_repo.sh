0f1bc20871490b8f6c4d466a1c6a028fc17c8b45
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-05-29 16:33:25 +0200

    Improve cleaning sript

--- clean_updated_repo.sh
+++ clean_updated_repo.sh
@@ -1,28 +1,29 @@
-#!/usr/bin/sh
+#!/bin/sh
 
-git rm -r ArcGIS_toolbox/
-git rm -r IMAGINE_toolbox/
-git rm -r LASlib/lib/
-git rm -r LASzip/dll/
-git rm -r QGIS_toolbox/
-git rm -r bin/
-git rm -r data/
-git rm -r example_batch_scripts/
-git rm -r src/
-git rm HALL_OF_SHAME.txt
-git rm LASlib/LASlib.dsp
-git rm LASlib/LASlib.dsw
-git rm LASlib/Makefile
-git rm LASlib/README.txt
-git rm LASlib/example/lasexample.dsp
-git rm LASlib/example/lasexample_add_rgb.dsp
-git rm LASlib/example/lasexample_write_only.dsp
-git rm LASzip/LASzip.dsp
-git rm LASzip/LASzip.dsw
-git rm LASzip/Makefile
-git rm LASzip/README.txt
-git rm LASzip/example/laszipdllexample.dsp
-git rm LASzip/example/laszippertest.dsp
-git rm Makefile
-git rm README.txt
-git rm lastools.dsw
+git rm -f -r ArcGIS_toolbox/
+git rm -f -r IMAGINE_toolbox/
+git rm -f -r LASlib/lib/
+git rm -f -r LASzip/dll/
+git rm -f -r QGIS_toolbox/
+git rm -f -r bin/
+git rm -f -r data/
+git rm -f -r example_batch_scripts/
+git rm -f -r src/
+git rm -f HALL_OF_SHAME.txt
+git rm -f LASlib/LASlib.dsp
+git rm -f LASlib/LASlib.dsw
+git rm -f LASlib/Makefile
+git rm -f LASlib/README.txt
+git rm -f LASlib/example/lasexample.dsp
+git rm -f LASlib/example/lasexample_add_rgb.dsp
+git rm -f LASlib/example/lasexample_write_only.dsp
+git rm -f LASzip/LASzip.dsp
+git rm -f LASzip/LASzip.dsw
+git rm -f LASzip/Makefile
+git rm -f LASzip/README.txt
+git rm -f LASzip/example/laszipdllexample.dsp
+git rm -f LASzip/example/laszippertest.dsp
+git rm -r LASzip/src/laszip_dll.cpp
+git rm -f Makefile
+git rm -f README.txt
+git rm -f lastools.dsw
